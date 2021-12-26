ea1b880e6a955308436c1d660182e32f42924f23
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2021-12-26 18:08:06 +0100

    Install packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './features/components/Home';
+
+//Switch was replaced with Routes !!!!
+//component was replaced with elements !!!!
+// don't need exact keyword
+
+// https://dev.to/gabrlcj/react-router-v6-some-of-the-new-changes-181m
+// https://www.robinwieruch.de/react-router/
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build slack today!</h1>
+      <Router>
+        <>
+          <Routes>
+            <Route  path="/" element={<Home/>}/>
+          </Routes>
+        </>
+      </Router>
     </div>
   );
 }
